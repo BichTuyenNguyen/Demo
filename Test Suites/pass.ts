d554cf57705f962ee9df40cc6bf2a759ef323d1a
tuyenKatalon <tuyen.nguyen@katalon.com>
2022-12-28 10:50:41 +0700

    update pass test suite

---
+++ Test Suites/pass.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>pass</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>2556b2a8-ad2a-4085-b7e1-aa47af5c5395</testSuiteGuid>
+   <testCaseLink>
+      <guid>6b2c04c1-87a9-4de1-964d-54b23eecea95</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/pass</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
