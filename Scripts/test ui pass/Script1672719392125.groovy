0fc63f579c3b5ce58f584576634e50fe78ace74f
tuyenKatalon <tuyen.nguyen@katalon.com>
2023-01-04 11:00:34 +0700

    update new test suite

---
+++ Scripts/test ui pass/Script1672719392125.groovy
@@ -0,0 +1,6 @@
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+WebUI.openBrowser('')
+assert 1==1
+WebUI.closeBrowser()
+
---
+++ Test Suites/test ui pass.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test ui pass</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9cef0e93-7841-47fa-a429-5f78b523ad2c</testSuiteGuid>
+   <testCaseLink>
+      <guid>b908c936-2896-4c3d-aee7-3d45493d89ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/test ui pass</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
